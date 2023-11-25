20ed203bd34001cb60976451fde6ecf6a56cc5be
v.kapustian <vladislav.kapustyan@immelman.ru>
2023-11-25 18:57:19 +0300

    Add lint

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/* eslint no-param-reassign: 0 */
 
-module.exports = nextConfig
+const path = require("path");
+// eslint-disable-next-line import/no-extraneous-dependencies
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  compress: true,
+  reactStrictMode: false,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/app/styles")],
+    prependData: "@import \"_variables.scss\";\n @import 'mixins';",
+    sourceMaps: true,
+  },
+};
+
+module.exports = withBundleAnalyzer(nextConfig);
