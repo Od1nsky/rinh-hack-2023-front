ee67de70aa005f79b140020cc57a79664c02073f
v.kapustian <vladislav.kapustyan@immelman.ru>
2023-11-25 20:23:34 +0300

    Add layout

--- next.config.js
+++ next.config.js
@@ -3,27 +3,27 @@
 const path = require("path");
 // eslint-disable-next-line import/no-extraneous-dependencies
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
+   enabled: process.env.ANALYZE === "true",
 });
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-  compress: true,
-  reactStrictMode: false,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
+   compress: true,
+   reactStrictMode: false,
+   webpack(config) {
+      config.module.rules.push({
+         test: /\.svg$/i,
+         issuer: /\.[jt]sx?$/,
+         use: ["@svgr/webpack"],
+      });
 
-    return config;
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, "src/app/styles")],
-    prependData: "@import \"_variables.scss\";\n @import 'mixins';",
-    sourceMaps: true,
-  },
+      return config;
+   },
+   sassOptions: {
+      includePaths: [path.join(__dirname, "src/global")],
+      prependData: '@import "_variables.scss";',
+      sourceMaps: true,
+   },
 };
 
 module.exports = withBundleAnalyzer(nextConfig);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import styles from "./page.module.css";
 
-export default function Home() {
-   return <main className={styles.main}>asdasd</main>;
-}
+const Home = () => <main className={styles.main}>Hello world</main>;
+
+export default Home;
